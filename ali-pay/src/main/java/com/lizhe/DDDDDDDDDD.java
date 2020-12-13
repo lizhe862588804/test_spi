795b136832c990148879b28baddaa8b281238e2f
lizhe <lizhe862588804@163.com>
2020-12-13 15:36:29 +0800

    二次提交 DD

---
+++ ali-pay/src/main/java/com/lizhe/DDDDDDDDDD.java
@@ -0,0 +1,13 @@
+package com.lizhe;
+
+/**
+ * @Auther: lizhe
+ * @Date: 2020/12/13 - 12 - 13 - 15:35
+ * @Description:com.lizhe
+ * @version: 1.0
+ */
+public class DDDDDDDDDD {
+    public static void main(String[] args) {
+        System.out.println("dddddddddddddddddd");
+    }
+}
