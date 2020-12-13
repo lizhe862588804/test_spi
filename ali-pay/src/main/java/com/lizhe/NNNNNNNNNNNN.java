65d2f9c65cc524155c94735177d8eac7bb603ffb
lizhe <lizhe862588804@163.com>
2020-12-13 15:52:15 +0800

    nnnnnnnnnnnnnnnnnnn

---
+++ ali-pay/src/main/java/com/lizhe/NNNNNNNNNNNN.java
@@ -0,0 +1,13 @@
+package com.lizhe;
+
+/**
+ * @Auther: lizhe
+ * @Date: 2020/12/13 - 12 - 13 - 15:51
+ * @Description:com.lizhe
+ * @version: 1.0
+ */
+public class NNNNNNNNNNNN {
+    public static void main(String[] args) {
+        System.out.println("nnnnnnnnnnnnnn");
+    }
+}
